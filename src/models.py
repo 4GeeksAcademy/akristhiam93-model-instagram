2531677700c16473beba93cbeb017b6a59d989f3
akristhiam93 <a.kristhiam93@gmail.com>
2026-03-23 17:54:43 +0000

    Diagram Completed

--- src/models.py
+++ src/models.py
@@ -7,13 +7,68 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    user: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    photo: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    post_id: Mapped[int] = mapped_column(primary_key=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Dm(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    message: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    reciever_id: Mapped[int] = mapped_column(primary_key=True)
+    sender_id: Mapped[int] = mapped_column(primary_key=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            # do not serialize the password, its a security breach
+        }
+    
+class Followers(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    seguido_id: Mapped[int] = mapped_column(primary_key=True)
+    seguidores_id: Mapped[int] = mapped_column(primary_key=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "seguido_id": self.seguido_id,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    publicacion: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            # do not serialize the password, its a security breach
+        }
+
